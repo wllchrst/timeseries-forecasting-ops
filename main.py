84e40e4dec356ac46e55301a2a31b43c05e3854b
William Christian <wllchrst@outlook.com>
2025-03-25 16:49:58 +0700

    feat main script for testing training

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 """
 import uvicorn
 from api import app
-from helper import EnvHelper
+from helper import env_helper
+
+"""Commodity"""
+from model.commodity import commodity_model, CommodityDataset
 
 if __name__ == '__main__':
-    env_helper = EnvHelper()
-    uvicorn.run(app, host=env_helper.api_host)
+    # uvicorn.run(app, host=env_helper.api_host)
+    commodity_dataset = CommodityDataset('./dataset/commodity/training.csv')
+    commodity_model.finetune_model(dataset=commodity_dataset.dataset)
