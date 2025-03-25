4803ad3912a93e99992e05863f233f0c89b0cc6b
William Christian <wllchrst@outlook.com>
2025-03-25 16:50:21 +0700

    feat: dataset and model class

---
+++ model/commodity/dataset.py
@@ -0,0 +1,33 @@
+"""Class for commodity dataset"""
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from model.commodity.constant import feature_to_scale
+class CommodityDataset:
+    def __init__(self, dataset_path: str):
+        self.dataset_path = dataset_path
+        self.dataset: pd.DataFrame = pd.read_csv(self.dataset_path)
+        self.labels: list[str] = []
+        self.province_encoder = LabelEncoder()
+        self.commodity_encoder = LabelEncoder()
+        self.scaler = StandardScaler()
+        self.process_dataset()
+        
+    def process_dataset(self) -> bool:
+        """
+        Process dataset with encoder and scaling
+        """
+        try:
+            self.dataset['Date'] = pd.to_datetime(self.dataset['Date'])
+            self.dataset['timestamp'] = self.dataset['Date'].astype('int64') // 10**9
+            self.dataset['province'] = self.province_encoder.fit_transform(self.dataset['province'])
+            self.dataset['commodity'] = self.commodity_encoder.fit_transform(self.dataset['commodity'])
+
+            self.dataset = self.dataset.drop(columns=['Unnamed: 0'])
+
+            for col in feature_to_scale:
+                self.dataset[col] = self.scaler.fit_transform(self.dataset[[col]])
+
+            self.province_mapping = {idx: label for idx, label in enumerate(self.province_encoder.classes_)}
+            self.commodity_mapping = {idx: label for idx, label in enumerate(self.commodity_encoder.classes_)}
+        except Exception as e:
+            print(f'Error processing dataset: {e}')
---
+++ model/commodity/model.py
@@ -0,0 +1,55 @@
+"""
+Class for holding commodity model
+"""
+import pandas as pd
+from model.commodity.training import fewshot_finetune_eval, predict_new_data
+from transformers import Trainer
+from tsfm_public import TimeSeriesPreprocessor
+class CommodityModel:
+    def __init__(self):
+        self.trainer: Trainer = None
+        self.tsp: TimeSeriesPreprocessor = None
+        self.context_length = 512
+        self.prediction_length = 96
+        
+    def finetune_model(self, dataset: pd.DataFrame, testing=False):
+        """Finetune model using the dataset given in the commodity model.   
+        """
+        try:
+            self.trainer, self.tsp = fewshot_finetune_eval(
+                training_dataset=dataset,
+                batch_size=32,
+                context_length=self.context_length,
+                forecast_length=self.prediction_length,
+                fewshot_percent=30,
+                learning_rate=0.001,
+                num_epochs=20 if not testing else 1,
+                save_dir='/save_dir'
+            )
+            print('Finetuning model success')
+        except Exception as e:
+            print(f'Finetuning model error: {e}')
+    
+    def test_with_dataset(self, test_dataset: pd.DataFrame):
+        """Test Finetune using test dataset in the parameter
+
+        Args:
+            test_dataset (pd.DataFrame): dataset that is going to predicted
+        """
+        if self.trainer is None or self.tsp is None:
+            print(f"Trainer have not been trained, cannot do testing for now!")
+
+        try:
+            predictions_df, _, _ = predict_new_data(
+                trainer=self.trainer,
+                new_data=test_dataset,
+                tsp=self.tsp,
+                context_length=self.context_length,
+                forecast_length=self.prediction_length,
+                dataset_name="new_prediction",
+                with_plot=False 
+            )
+            
+            print(predictions_df)
+        except Exception as e:
+            print(f'Dataset testing error: {e}')
