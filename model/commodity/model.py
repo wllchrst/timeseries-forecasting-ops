1b277126e96493619fb9d824e32f4f930cb0d174
William Christian <wllchrst@outlook.com>
2025-03-27 15:40:02 +0700

    feat: predict new data done

--- model/commodity/model.py
+++ model/commodity/model.py
@@ -1,6 +1,7 @@
 """
 Class for holding commodity model
 """
+import helper
 import pandas as pd
 import numpy as np
 from transformers import Trainer
@@ -13,23 +14,34 @@ class CommodityModel:
     def __init__(self):
         self.trainer: Trainer = None
         self.tsp: TimeSeriesPreprocessor = None
+        self.province_mapping: dict = None
+        self.commodity_mapping: dict = None
         self.context_length = 512
         self.prediction_length = 96
 
-    def finetune_model(self, dataset: pd.DataFrame, testing=False):
+    def finetune_model(
+        self,
+        dataset: pd.DataFrame,
+        province_mapping: dict,
+        commodity_mapping: dict,
+        testing=False):
         """Finetune model using the dataset given in the commodity model.
         """
+        dummy_dataset = dataset[0: 500]
         try:
             self.trainer, self.tsp = fewshot_finetune_eval(
-                training_dataset=dataset,
+                training_dataset=dummy_dataset if testing else dataset,
                 batch_size=32,
                 context_length=self.context_length,
                 forecast_length=self.prediction_length,
                 fewshot_percent=30,
                 learning_rate=0.001,
-                num_epochs=20 if not testing else 1,
+                num_epochs=1 if testing else 20,
                 save_dir='./save_dir'
             )
+            
+            self.province_mapping = province_mapping
+            self.commodity_mapping = commodity_mapping
             print('Finetuning model success')
         except Exception as e:
             print(f'Finetuning model error: {e}')
@@ -67,4 +79,47 @@ class CommodityModel:
         Returns:
             np.float64: Price for that day
         """
-        pass
+        if self.tsp is None:
+            return -1
+        
+        try:
+            predict_df = self.create_df(commodity_price_request)
+        
+            results_df, _, _ = predict_new_data(
+                trainer=self.trainer,
+                new_data=predict_df,
+                tsp=self.tsp,
+                context_length=self.context_length,
+                forecast_length=self.prediction_length,
+                dataset_name="new_prediction",
+                with_plot=False
+            )
+            print(results_df.head())
+        except Exception as e:
+            print(f'Predicting price errpr: {e}')
+
+    def create_df(
+        self,
+        commodity_price_request: CommodityPriceRequestDTO,
+    ) -> pd.DataFrame:
+        dict = {
+            'Date': [pd.to_datetime(commodity_price_request.date)],
+            'commodity': [self.commodity_mapping[commodity_price_request.commodity]],
+            'province': [self.province_mapping[commodity_price_request.province]],
+            'GlobalOpen': [commodity_price_request.global_open],
+            'GlobalHigh': [commodity_price_request.global_high],
+            'GlobalLow': [commodity_price_request.global_low],
+            'GlobalVol.': [commodity_price_request.global_volume],
+            'GlobalChange %': [commodity_price_request.global_change_percent],
+            'GlobalPrice': [commodity_price_request.global_price],
+            'CE_Close': [commodity_price_request.ce_close],
+            'CE_High': [commodity_price_request.ce_high],
+            'CE_Low': [commodity_price_request.ce_low],
+            'CE_Open': [commodity_price_request.ce_open],
+            'GTPrice': [commodity_price_request.gt_price],
+            'price': [0],
+        }
+        
+        df = pd.DataFrame(dict)
+        df['timestamp'] = df['Date'].astype('int64') // 10**9
+        return df
