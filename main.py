899076acc7b1f048caafa22b789e7c61765a35eb
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-03-28 15:12:01 +0700

    feat: main class for starting everything

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 """Main Script for running the project
 """
-import uvicorn
-from api import app
-from helper import env_helper
+from helper import EnvHelper, DataHelper
+from model import CommodityHandler
 
-"""Commodity"""
-from model.commodity import commodity_model, CommodityDataset
+class Main:
+    def __init__(self):
+        EnvHelper()
+        DataHelper()
+        self.start_commodity()
+    
+    def start_commodity(self):
+        self.commodity_handler = CommodityHandler(True)
 
 if __name__ == '__main__':
-    # uvicorn.run(app, host=env_helper.api_host)
-    commodity_dataset = CommodityDataset('./dataset/commodity/training.csv')
-    commodity_model.finetune_model(dataset=commodity_dataset.dataset)
+    Main()
