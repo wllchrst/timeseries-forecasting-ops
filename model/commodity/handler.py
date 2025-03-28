6c8badf99b7102d1410a5bd2579876c36600593e
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-03-28 15:11:19 +0700

    feat: handler for commodity done

--- model/commodity/handler.py
+++ model/commodity/handler.py
@@ -2,31 +2,33 @@
 import pandas as pd
 from model.commodity.model import CommodityModel
 from model.commodity.dataset import CommodityDataset
-from helper import env_helper
+from api.types import CommodityPriceRequestDTO
+from helper import DataHelper
+from app_decorator import singleton
+@singleton
 class CommodityHandler:
     def __init__(self, testing=False):
+        self.data_helper = DataHelper()
         self.model = CommodityModel()
         self.testing = testing
-        self.gather_initial_dataset()
         self.finetune_and_evaluate()
-
-    def gather_initial_dataset(self):
-        try:
-            self.train_dset = CommodityDataset(env_helper.traing_dset_path)
-            self.test_dset = CommodityDataset(env_helper.testing_dset_path, True)
-        except Exception as e:
-            print(f'Gathering initial dataset: {e}')
 
     def finetune_and_evaluate(self):
         try:
+            self.train_dset = CommodityDataset(self.data_helper.commodity_initial_training)
+            self.test_dset = CommodityDataset(self.data_helper.commodity_initial_testing, True)
+
             self.model.finetune_model(
-                dataset=self.train_dset,
+                dataset=self.train_dset.dataset,
                 commodity_mapping=self.train_dset.commodity_mapping,
                 province_mapping=self.train_dset.province_mapping,
                 testing=self.testing
             )
-            
-            self.model.test_with_dataset(self.test_dset)
-            
+
+            self.model.test_with_dataset(self.test_dset.dataset)
+
         except Exception as e:
             print(f'Finetuning and evaluate error: {e}')
+
+    def predict_price(self, commodity_request_dto: CommodityPriceRequestDTO):
+        self.model.predict_price(commodity_request_dto)
