ad735e2361f5b55dbde32c95b56918678d09ff39
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-04-02 09:55:13 +0700

    feat: collect additional env

--- helper/env_helper.py
+++ helper/env_helper.py
@@ -19,5 +19,7 @@ class EnvHelper:
             self.api_host = os.getenv("API_HOST")
             self.training_dset_path= os.getenv("COMMODITY_TRAINING")
             self.testing_dset_path = os.getenv("COMMODITY_TESTING")
+            self.model_tracking_uri = os.getenv("MODEL_TRACKING_URI")
+            self.commodity_training_parameter_path = os.getenv("COMMODITY_TRAINING_PARAMETER_PATH")
         except Exception as e:
             print(f'Gathering Env failed: {e}')
